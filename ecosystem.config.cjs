5da135ed34e0491e10cce7f3a6769f3424fd8e4c
enterpannet <enterpannet@gmail.com>
2024-08-25 18:55:52 +0700

    deploy 1.1

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
       args: '-s dist',  // The '-s' flag indicates serving a single-page application
       env: {
         PM2_SERVE_PATH: './dist',
-        PM2_SERVE_PORT: 4173,
+        PM2_SERVE_PORT: 3173,
         PM2_SERVE_SPA: 'true',
         PM2_SERVE_HOMEPAGE: '/index.html'
       }
