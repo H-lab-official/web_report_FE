d8172a00bb8ca16974bb5db89be408d352bc0145
enterpannet <enterpannet@gmail.com>
2024-08-26 06:00:33 +0700

    deploy 2.07

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['react-router-dom'],
+
     },
     outDir: 'dist', // Ensure the output directory is set correctly
     assetsDir: 'assets', // Ensure assets like JS and CSS are correctly placed
